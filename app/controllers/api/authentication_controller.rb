716681a090f2fde2456687d25a3f3a9c55c9e514
Hossam-Hawary <hossameldeen.hawary@gmail.com>
2017-11-06 23:27:30 +0200

    primit user_params

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -2,7 +2,7 @@ class Api::AuthenticationController < ApplicationController
       skip_before_action :authenticate_request,:verify_authenticity_token, only: :authenticate
 
 	def authenticate
-    	command = AuthenticateUser.call(params[:email], params[:password])
+    	command = AuthenticateUser.call(user_params[:email], user_params[:password])
     	# user = Api::User.find_by_email params[:email]
     	 # render json: { auth_token: user.authenticate(params[:password]) }
 
@@ -10,13 +10,19 @@ class Api::AuthenticationController < ApplicationController
     	if command.success?
     		 render json: { auth_token: command.result }
     	else
-    		render json: { error: command.errors, params:params }, status: :unauthorized
+    		render json: { error: command.errors, params:user_params }, status: :unauthorized
     	end
 
     end
 
     def need_auth
     	 render json: { success:true }
+    end
+    ####################
+    private
+    #####################
+    def user_params
+      params.require(:user).permit(:email, :password)
     end
 
 end
