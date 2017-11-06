0f344b52dc3d4afde5c2e02b0c90a6349e588e86
Hossam-Hawary <hossameldeen.hawary@gmail.com>
2017-11-06 23:20:29 +0200

    verify_authenticity_token auth controller

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,10 +1,9 @@
 class Api::AuthenticationController < ApplicationController
       skip_before_action :authenticate_request,:verify_authenticity_token, only: :authenticate
-      skip_before_action :verify_authenticity_token
 
 	def authenticate
     	command = AuthenticateUser.call(params[:email], params[:password])
-    	user = Api::User.find_by_email params[:email]
+    	# user = Api::User.find_by_email params[:email]
     	 # render json: { auth_token: user.authenticate(params[:password]) }
 
 
